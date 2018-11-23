35150769dc6d8d77ec370639b0aeaeeadc10b8dd
Andrei Sannikov <andrei.sannikov.82@mail.ru>
2018-11-23 03:37:42 +0300

    Переписал инициализацию ШИМ, Убрал прерывание, добавил инверсию к отрицательному вольтажу

--- src/lab_functions/motor_voltage.c
+++ src/lab_functions/motor_voltage.c
@@ -21,27 +21,26 @@ Local defines
 /***************************************************************************************************
 Локальные дефайны
 ***************************************************************************************************/
-#define TIMER_PRESCALER		720
-#define EXT_TIM_PULSE 	150
-#define TIM_PULSE		50
 /***************************************************************************************************
 Local data types
 ***************************************************************************************************/
 /***************************************************************************************************
 Локальные типы данных
 ***************************************************************************************************/
-
+#define SYSCLK 72000000
+#define PRESCALER 72
 /***************************************************************************************************
 File local variables
 ***************************************************************************************************/
 /***************************************************************************************************
 Локальные переменные файла
 ***************************************************************************************************/
-uint16_t previousState;
 GPIO_InitTypeDef port;
 TIM_TimeBaseInitTypeDef timer;
 TIM_OCInitTypeDef timerPWM;
-uint16_t buttonPreviousState;
+ 
+int sound_time;
+int sound_counter;
 /***************************************************************************************************
 Global functions
 ***************************************************************************************************/
@@ -57,49 +56,50 @@ Global functions
 void PWM_init()
 {
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM1, ENABLE);
  
-    GPIO_InitTypeDef port;
+    GPIO_StructInit(&port);
     port.GPIO_Mode = GPIO_Mode_AF_PP;
     port.GPIO_Pin = GPIO_Pin_8;
+	
+    port.GPIO_Mode = GPIO_Mode_Out_PP;
+    port.GPIO_Pin = GPIO_Pin_11;
+	
     port.GPIO_Speed = GPIO_Speed_2MHz;
     GPIO_Init(GPIOA, &port);
 	
-	  const uint16_t period = 1000; // Period 1s (period = 1000 ticks of timer i.e. 1 seconds)
-		const uint16_t freq = 1000; // Frequency 1ms (1 timer tick per 1 milisecond)
+    TIM_TimeBaseStructInit(&timer);
+    timer.TIM_Prescaler = PRESCALER;
+    timer.TIM_Period = SYSCLK / PRESCALER / 2000;
+    timer.TIM_ClockDivision = 0;
+    timer.TIM_CounterMode = TIM_CounterMode_Up;
+		TIM_TimeBaseInit(TIM1, &timer);	
  
-		TIM_TimeBaseInitTypeDef structTIM;
-		TIM_TimeBaseStructInit(&structTIM);
-		structTIM.TIM_Period = period - 1;
-		structTIM.TIM_Prescaler = SystemCoreClock / freq - 1;
-		TIM_TimeBaseInit(TIM1, &structTIM);	
- 
-    TIM_OCInitTypeDef timerPWM;
-    timerPWM.TIM_Pulse = 50;
+    TIM_OCStructInit(&timerPWM);
     timerPWM.TIM_OCMode = TIM_OCMode_PWM1;
     timerPWM.TIM_OutputState = TIM_OutputState_Enable;
+    timerPWM.TIM_OCPolarity = TIM_OCPolarity_High;
     TIM_OC1Init(TIM1, &timerPWM);
 	
-		// Configure timer on interrupting by update 
-		TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
-		//	NVIC_SetPriority(TIM1_UP_IRQn, 2);
-		// Enable interrupting from TIM1
-		NVIC_EnableIRQ(TIM1_UP_IRQn);
-	
-		// Turns on timer
-		TIM_Cmd(TIM1, ENABLE);
+//		// Configure timer on interrupting by update 
+//		TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
+//		//	NVIC_SetPriority(TIM1_UP_IRQn, 2);
+//		// Enable interrupting from TIM1
+//		NVIC_EnableIRQ(TIM1_UP_IRQn);
+//	
+//		// Turns on timer
+//		TIM_Cmd(TIM1, ENABLE);
 }
-/**************************************************************************************************
-Описание: Инициализация USART 
-Аргументы: Новое значение напряжения на моторе в диапазоне от -1000 до 1000
-Возврат:   Нет
-Замечания: Измеряется в мВ ?
-**************************************************************************************************/
-void TIM1_IRQHandler()
-{	
 
-} 
 void motor_voltage_setVoltage(int16_t voltage)
 {
+	if (voltage < 0)
+	{
+		GPIO_SetBits(GPIOA, GPIO_Pin_11)
+    TIM1->CCR1 = 2*voltage;
+	}
+	else
+	{
+		TIM1->CCR1 = 2*voltage;
+	}		
 }
-
